1f45c8af1082fbeabe57bb4dae181dc9af13d50b
Gabriel Notman <g.notman@gmail.com>
2019-03-26 14:00:52 +0100

    Some formatting and whitespace changes

--- developer-kit/Hiber.cpp
+++ developer-kit/Hiber.cpp
@@ -11,19 +11,19 @@ static Stream* debug_port = NULL;
 
 void setDebugSerial(Stream* port)
 {
-  debug_port = port;
+    debug_port = port;
 }
 
 void toggleDebugIO(bool enabled)
 {
-  debug_io = enabled;
+    debug_io = enabled;
 }
 
 String getHex(char x)
 {
-  String ret = String(x, HEX);
-  if (x < 0x10) ret = "0" + ret;
-  return ret;
+    String ret = String(x, HEX);
+    if (x < 0x10) ret = "0" + ret;
+    return ret;
 }
 
 void flushInput(Stream *stream, Stream *debug)
@@ -38,310 +38,312 @@ void flushInput(Stream *stream, Stream *debug)
 
 String readln(Stream *stream)
 {
-  String tmp = "";
-  tmp.reserve(20);
-  char prev = 0;
-  unsigned long start_time = millis();
-  
-  if (debug_io) ::writeString(debug_port, "< ");
-  
-  while (true) {
+    String tmp = "";
+    tmp.reserve(20);
+    char prev = 0;
+    unsigned long start_time = millis();
+
+    if (debug_io) ::writeString(debug_port, "< ");
+
+    while (true) {
         if (!stream->available()) {
-      // Detect timeout
-      if ((millis() - start_time) >= 7000) {
-        ::writeString(debug_port, "timeout\r\n");
-        return tmp;
-      }
-      continue;
-    }
+            // Detect timeout
+            if ((millis() - start_time) >= 7000) {
+                ::writeString(debug_port, "timeout\r\n");
+                return tmp;
+            }
+            continue;
+        }
         else {
             char cur = stream->read();
             if (debug_io) {
                 //debug_port->print(getHex(cur));
             }
 
-    if (prev == '\r') {
-      if (cur == '\n') {
-        return tmp;
-      }
-    }
-    else if (cur != '\r') {
-      tmp += cur;
+            if (prev == '\r') {
+                if (cur == '\n') {
+                    return tmp;
+                }
+            }
+            else if (cur != '\r') {
+                tmp += cur;
+            }
+
+            prev = cur;
+        }
     }
 
-    prev = cur;
-  }
-    }
-
-  return tmp;
+    return tmp;
 }
 
 // This function will write a String to a Stream,
 // byte-for-byte, and then flushes it.
 void writeString(Stream *stream, String value)
 {
-  if (stream == nullptr) {
-    // Lets not try to access a null pointer
-    return;
-  }
-  stream->print(value);
-  stream->flush();
+    if (stream == nullptr) {
+        // Lets not try to access a null pointer
+        return;
+    }
+    stream->print(value);
+    stream->flush();
 }
 
 void Hiber::writeString(String value)
 {
-  if (debug_io) ::writeString(debug_port, "> " + value);
-  ::writeString(serial_port, value);
+    if (debug_io) ::writeString(debug_port, "> " + value);
+    ::writeString(serial_port, value);
 }
 
 Hiber::Hiber(Stream *serial_port)
 {
-  this->serial_port = serial_port;
-  this->command_with_arguments = false;
+    this->serial_port = serial_port;
+    this->command_with_arguments = false;
 }
 
 // This function replaces the current command buffer
 // with 'command' as its only contents
 void Hiber::sendCommandName(String command)
 {
-  last_command = command;
+    last_command = command;
 }
 
 // This function will either add '(' or ',', depending
 // on whether an argument was already started
 void Hiber::sendArgumentStart()
 {
-  if (!command_with_arguments) {
-    command_with_arguments = true;
-    last_command += '(';
-  } else {
-    last_command += ',';
-  }
+    if (!command_with_arguments) {
+        command_with_arguments = true;
+        last_command += '(';
+    }
+    else {
+        last_command += ',';
+    }
 }
 
 void Hiber::sendCommandFinish()
 {
-  if (command_with_arguments) {
-    last_command += ')';
-  }
+    if (command_with_arguments) {
+        last_command += ')';
+    }
 
-  last_command += "\r\n";
+    last_command += "\r\n";
 
     flushInput(serial_port, debug_port);
-  writeString(last_command);
-  command_with_arguments = false;
+    writeString(last_command);
+    command_with_arguments = false;
 }
 
 void Hiber::sendArgumentString(String value)
 {
-  sendArgumentStart();
+    sendArgumentStart();
 
-  last_command += '"';
+    last_command += '"';
 
-  int length = value.length();
-  for (int i = 0; i < length; i++) {
-    char c = value[i];
+    int length = value.length();
+    for (int i = 0; i < length; i++) {
+        char c = value[i];
 
-    // Handle raw characters
-    if (c == '\r') {
-      last_command += "\\r";
-      continue;
+        // Handle raw characters
+        if (c == '\r') {
+            last_command += "\\r";
+            continue;
+        }
+        if (c == '\n') {
+            last_command += "\\n";
+            continue;
+        }
+
+        // Escape special characters
+        if (c == '\\' || c == '"' || c == '\'') {
+            last_command += '\\';
+        }
+
+        last_command += c;
     }
-    if (c == '\n') {
-      last_command += "\\n";
-      continue;
-    }
 
-    // Escape special characters
-    if (c == '\\' || c == '"' || c == '\'') {
-      last_command += '\\';
-    }
-
-    last_command += c;
-  }
-
-  last_command += '"';
+    last_command += '"';
 }
 
 void Hiber::sendArgumentInt(int value, bool hex)
 {
-  sendArgumentStart();
-  String characters = String(value, hex ? HEX : DEC);
+    sendArgumentStart();
+    String characters = String(value, hex ? HEX : DEC);
 
-  if (hex) {
-    last_command += "0x";
-  }
+    if (hex) {
+        last_command += "0x";
+    }
 
-  last_command += characters;
+    last_command += characters;
 }
 
 void Hiber::sendArgumentBool(bool value)
 {
-  sendArgumentStart();
-  last_command += (value == true ? "true" : "false");
+    sendArgumentStart();
+    last_command += (value == true ? "true" : "false");
 }
 
 void Hiber::sendArgumentFloat(float value)
 {
-  String characters = String(value, 5); // decimals as second arg
-  // Floats are strings ("123.456")
-  sendArgumentString(characters);
+    String characters = String(value, 5); // decimals as second arg
+    // Floats are strings ("123.456")
+    sendArgumentString(characters);
 }
 
 static String parseStringArgument(String str, int *nextOffset, bool *valid)
 {
-  if (str[0] != '"') {
-    // Ehm...
-    *nextOffset = -1;
-    *valid = false;
-    return "";
-  }
-
-  int nextQuote;
-  int nextQuoteMinOffset = 1;
-
-  do {
-    nextQuote = str.indexOf('"', nextQuoteMinOffset);
-
-    if (nextQuote == -1) {
-      // Invalid contents
-      *nextOffset = -1;
-      *valid = false;
-      return "";
+    if (str[0] != '"') {
+        // Ehm...
+        *nextOffset = -1;
+        *valid = false;
+        return "";
     }
 
-    if (nextQuote > 1) {
-      // See if previous value is a slash
-      if (str[nextQuote - 1] == '\\') {
-        // Ignore.
-        nextQuoteMinOffset = nextQuote + 1;
-        continue;
-      }
-      
-      // Oh? Not a slash? We should be at the end, then.
-    }
-    
+    int nextQuote;
+    int nextQuoteMinOffset = 1;
 
-    if (str[nextQuote + 1] == ')') {
-      // Yes, pretty sure we are at the end.
-      // Make sure we communicate this back
-      *nextOffset = -1;
-    } else if (str[nextQuote + 1] == ';') {
-      // Okay, more stuff to handle
-      *nextOffset = nextQuote + 1;
-    }
+    do {
+        nextQuote = str.indexOf('"', nextQuoteMinOffset);
 
-    *valid = true;
-    // Return the string
-    return str.substring(1, nextQuote - 2);
-  } while (true);
-  
+        if (nextQuote == -1) {
+            // Invalid contents
+            *nextOffset = -1;
+            *valid = false;
+            return "";
+        }
+
+        if (nextQuote > 1) {
+            // See if previous value is a slash
+            if (str[nextQuote - 1] == '\\') {
+                // Ignore.
+                nextQuoteMinOffset = nextQuote + 1;
+                continue;
+            }
+
+            // Oh? Not a slash? We should be at the end, then.
+        }
+
+
+        if (str[nextQuote + 1] == ')') {
+            // Yes, pretty sure we are at the end.
+            // Make sure we communicate this back
+            *nextOffset = -1;
+        }
+        else if (str[nextQuote + 1] == ';') {
+            // Okay, more stuff to handle
+            *nextOffset = nextQuote + 1;
+        }
+
+        *valid = true;
+        // Return the string
+        return str.substring(1, nextQuote - 2);
+    } while (true);
+
 }
 
 short Hiber::parseResponse(String response, String arguments[], int max_argument_count, int *argument_count)
 {
-  if (response.length() < 7) {
-    return INVALID_RESPONSE_TOO_SHORT;
-  }
-  
-  if (debug_io) ::writeString(debug_port, "+> " + response);
+    if (response.length() < 7) {
+        return INVALID_RESPONSE_TOO_SHORT;
+    }
 
-  // Just make sure we begin at a valid API(, in case of
-  // random stuff in front of it.
-  int apiStartIndexOf = response.indexOf("API(");
-  if (apiStartIndexOf != -1) {
-    response = response.substring(apiStartIndexOf);
-  }
-  
-  if (debug_io) ::writeString(debug_port, " ..   " + response + " <+\r\n");
-  
-  if (response.startsWith("API(")) {
-    String codeStr = response.substring(4, 7);
-    short code = (short)codeStr.toInt();
-    int arg_count = 0;
+    if (debug_io) ::writeString(debug_port, "+> " + response);
 
-    if (response[7] == ':' && max_argument_count > 0) {
-      // We've got a reponse with arguments: API(123: foo; 1; "bar")
-      String args = response.substring(3 + 1 + 3 + 2); // skip API(123:
+    // Just make sure we begin at a valid API(, in case of
+    // random stuff in front of it.
+    int apiStartIndexOf = response.indexOf("API(");
+    if (apiStartIndexOf != -1) {
+        response = response.substring(apiStartIndexOf);
+    }
 
-      bool stop = false;
-      for (int i = 0; i < max_argument_count; i++) {
-        // Check if we've got an argument delimiter
-        int offsetArgTerminator = args.indexOf(';');
-        if (offsetArgTerminator == -1) {
-          // Apparently not, check for end-of-arguments character
-          offsetArgTerminator = args.indexOf(')');
-          if (offsetArgTerminator == -1) {
-            // Odd, only received half of the command
-            return INVALID_RESPONSE_NO_END;
-          }
-          // Signal that we have only 1 left
-          stop = true;
+    if (debug_io) ::writeString(debug_port, " ..   " + response + " <+\r\n");
+
+    if (response.startsWith("API(")) {
+        String codeStr = response.substring(4, 7);
+        short code = (short)codeStr.toInt();
+        int arg_count = 0;
+
+        if (response[7] == ':' && max_argument_count > 0) {
+            // We've got a reponse with arguments: API(123: foo; 1; "bar")
+            String args = response.substring(3 + 1 + 3 + 2); // skip API(123:
+
+            bool stop = false;
+            for (int i = 0; i < max_argument_count; i++) {
+                // Check if we've got an argument delimiter
+                int offsetArgTerminator = args.indexOf(';');
+                if (offsetArgTerminator == -1) {
+                    // Apparently not, check for end-of-arguments character
+                    offsetArgTerminator = args.indexOf(')');
+                    if (offsetArgTerminator == -1) {
+                        // Odd, only received half of the command
+                        return INVALID_RESPONSE_NO_END;
+                    }
+                    // Signal that we have only 1 left
+                    stop = true;
+                }
+
+                if (args[0] == '"') {
+                    if (debug_io) ::writeString(debug_port, "Reading string\r\n");
+                    int nextOffset = 0;
+                    bool valid = false;
+                    String argument = parseStringArgument(args, &nextOffset, &valid);
+                    if (!valid) {
+                        return INVALID_RESPONSE_STRING_ARG;
+                    }
+
+                    arguments[i] = argument;
+                    arg_count++;
+                    if (nextOffset == -1) stop = true;
+                    else offsetArgTerminator = nextOffset;
+                }
+                else {
+                    // Read until the offsetArgTerminator
+                    arguments[i] = args.substring(0, offsetArgTerminator);
+                    arg_count++;
+                }
+
+
+                if (stop) break;
+                args = args.substring(offsetArgTerminator + 2);
+            }
+
         }
 
-        if (args[0] == '"') {
-          if (debug_io) ::writeString(debug_port, "Reading string\r\n");
-          int nextOffset = 0;
-          bool valid = false;
-          String argument = parseStringArgument(args, &nextOffset, &valid);
-          if (!valid) {
-            return INVALID_RESPONSE_STRING_ARG;
-          }
+        *argument_count = arg_count;
 
-          arguments[i] = argument;
-          arg_count++;
-          if (nextOffset == -1) stop = true;
-          else offsetArgTerminator = nextOffset;
+        return code;
+    }
+    else if (response.startsWith("Hiber API (")) {
+        char commitHash[50];
+
+        int results = sscanf(response.c_str(), "Hiber API (Build %s @", &commitHash);
+
+        if (results == 1) {
+            // Okay, commit hash loaded
+            String rest = response.substring(response.indexOf("@") + 2);
+            String buildDate = rest.substring(0, rest.indexOf(')'));
+
+            ::writeString(debug_port, "Booted, " + String(commitHash) + " build at " + buildDate + "\r\n");
+
+            return INVALID_RESPONSE_DEVICE_JUST_BOOTED;
         }
         else {
-          // Read until the offsetArgTerminator
-          arguments[i] = args.substring(0, offsetArgTerminator);
-          arg_count++;
+            if (debug_io) ::writeString(debug_port, "Error\r\n");
         }
-
-
-        if (stop) break;
-        args = args.substring(offsetArgTerminator + 2);
-      }
-      
     }
-
-    *argument_count = arg_count;
-
-    return code;
-  }
-  else if (response.startsWith("Hiber API (")) {
-    char commitHash[50];
-    
-    int results = sscanf(response.c_str(), "Hiber API (Build %s @", &commitHash);
-
-    if (results == 1) {
-      // Okay, commit hash loaded
-      String rest = response.substring(response.indexOf("@") + 2);
-      String buildDate = rest.substring(0, rest.indexOf(')'));
-      
-      ::writeString(debug_port, "Booted, " + String(commitHash) + " build at " + buildDate + "\r\n");
-
-      return INVALID_RESPONSE_DEVICE_JUST_BOOTED;
-    }
-    else {
-      if (debug_io) ::writeString(debug_port, "Error\r\n");
-    }
-  }
-  return INVALID_RESPONSE_NO_BEGIN;
+    return INVALID_RESPONSE_NO_BEGIN;
 }
 
 // Read and parse a response from the Hiber modem
 short Hiber::readResponse(String arguments[], int max_argument_count, int *argument_count)
 {
-  String str = readln(serial_port);
-  previous_response = str;
+    String str = readln(serial_port);
+    previous_response = str;
 
-  if (debug_io) ::writeString(debug_port, "Input: " + previous_response + "\r\n");
+    if (debug_io) ::writeString(debug_port, "Input: " + previous_response + "\r\n");
 
-  short code = parseResponse(previous_response, arguments, max_argument_count, argument_count);
-  
-  if (code == INVALID_RESPONSE_DEVICE_JUST_BOOTED && last_command.length() != 0) {
+    short code = parseResponse(previous_response, arguments, max_argument_count, argument_count);
+
+    if (code == INVALID_RESPONSE_DEVICE_JUST_BOOTED && last_command.length() != 0) {
         if (debug_io) ::writeString(debug_port, "Trying to read next line...\r\n");
         
         str = readln(serial_port);
@@ -351,16 +353,15 @@ short Hiber::readResponse(String arguments[], int max_argument_count, int *argum
 
         if (code != RESPONSE_OK) {
             if (debug_io) ::writeString(debug_port, "Retrying command...\r\n");
-    writeString(last_command);
-    
-    str = readln(serial_port);
-    previous_response = str;
+            writeString(last_command);
 
-    code = parseResponse(previous_response, arguments, max_argument_count, argument_count);
-  }
+            str = readln(serial_port);
+            previous_response = str;
 
-  previous_response_code = code;
-  return code;
+            code = parseResponse(previous_response, arguments, max_argument_count, argument_count);
+        }
+    }
+
+    previous_response_code = code;
+    return code;
 }
-
-
--- developer-kit/Hiber.h
+++ developer-kit/Hiber.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef HIBER_H_
+#define HIBER_H_
 
 #include <Arduino.h>
 
@@ -8,47 +9,47 @@
 
 class Hiber {
 public:
-  typedef enum : byte {
-    CT_INFO = 0,
-    CT_ERROR = 1,
-    CT_DEBUG = 2,
-  } CodeTypes;
+    typedef enum : byte {
+        CT_INFO = 0,
+        CT_ERROR = 1,
+        CT_DEBUG = 2,
+    } CodeTypes;
 
-  typedef enum : byte {
-    CC_COMMAND_PARSING = 1,
-    CC_STRING_INTERPOLATION = 2,
-    CC_ARGUMENT_CONVERSION = 3,
-    CC_COMMAND_HANDLING = 4,
-    CC_RAW_INPUT_HANDLING = 5,
-    CC_COMMAND_EXECUTION = 6,
-  } CodeCategories;
+    typedef enum : byte {
+        CC_COMMAND_PARSING = 1,
+        CC_STRING_INTERPOLATION = 2,
+        CC_ARGUMENT_CONVERSION = 3,
+        CC_COMMAND_HANDLING = 4,
+        CC_RAW_INPUT_HANDLING = 5,
+        CC_COMMAND_EXECUTION = 6,
+    } CodeCategories;
 
-  typedef enum : short {
-    RESPONSE_OK = 600,
-    RESPONSE_STARTING_TO_SLEEP = 602,
-    RESPONSE_CANNOT_SLEEP_WKUP0_HIGH = 603,
-    RESPONSE_ERROR_GPS_IS_ENABLED = 633,
+    typedef enum : short {
+        RESPONSE_OK = 600,
+        RESPONSE_STARTING_TO_SLEEP = 602,
+        RESPONSE_CANNOT_SLEEP_WKUP0_HIGH = 603,
+        RESPONSE_ERROR_GPS_IS_ENABLED = 633,
 
-    // Arduino response processing error codes
-    INVALID_RESPONSE_TOO_SHORT = 950, // Unable to parse response (too short to be valid)
-    INVALID_RESPONSE_NO_END = 951, // Unable to parse response, because it had no termination character
-    INVALID_RESPONSE_STRING_ARG = 952, // Unable to parse a string in the response
-    INVALID_RESPONSE_NO_BEGIN = 953, // Unable to parse response (did not start with API( )
-    INVALID_RESPONSE_DEVICE_JUST_BOOTED = 954, // Received booting message
-  } Codes;
+        // Arduino response processing error codes
+        INVALID_RESPONSE_TOO_SHORT = 950, // Unable to parse response (too short to be valid)
+        INVALID_RESPONSE_NO_END = 951, // Unable to parse response, because it had no termination character
+        INVALID_RESPONSE_STRING_ARG = 952, // Unable to parse a string in the response
+        INVALID_RESPONSE_NO_BEGIN = 953, // Unable to parse response (did not start with API( )
+        INVALID_RESPONSE_DEVICE_JUST_BOOTED = 954, // Received booting message
+    } Codes;
 
-  typedef enum : byte {
-    GTSR_OK = 0,
-    GTSR_OK_ONLY_WKUP0 = 1,
-    GTSR_ERROR = 10,
-    GTSR_WAKEUP0_HIGH = 11,
-  } GoToSleepResult;
+    typedef enum : byte {
+        GTSR_OK = 0,
+        GTSR_OK_ONLY_WKUP0 = 1,
+        GTSR_ERROR = 10,
+        GTSR_WAKEUP0_HIGH = 11,
+    } GoToSleepResult;
 
-  Hiber(Stream *serial_port);
+    Hiber(Stream *serial_port);
 
-  // Actual commands. You can find the declarations inside HiberCalls.cpp
-  bool setGPSMode(bool enabled);
-  bool doGPSFix();
+    // Actual commands. You can find the declarations inside HiberCalls.cpp
+    bool setGPSMode(bool enabled);
+    bool doGPSFix();
     bool getFirmwareVersion(String *firmware_version);
     bool getModemInfo(String *HW_type_str, int *HW_type_int, int *FW_version, String *modem_no_str, int *modem_no_int);
     bool setModemNumber(String modem_number);
@@ -59,50 +60,50 @@ public:
     bool setPayload(byte data[], int data_len);
     bool getNextAlarm(int *reason, int *seconds_left);
     bool getNextPass(long *seconds_left);
-  bool goToSleep(GoToSleepResult *result, int *reason, int *seconds_left);
+    bool goToSleep(GoToSleepResult *result, int *reason, int *seconds_left);
     bool togglePayloadOverDebug(bool enabled);
 
-  // Functionality for building commands
-  void sendCommandName(String command);
-  void sendCommandFinish();
+    // Functionality for building commands
+    void sendCommandName(String command);
+    void sendCommandFinish();
 
-  void sendArgumentString(String value);
-  void sendArgumentInt(int value, bool hex = false);
-  void sendArgumentBool(bool value);
-  void sendArgumentFloat(float value);
+    void sendArgumentString(String value);
+    void sendArgumentInt(int value, bool hex = false);
+    void sendArgumentBool(bool value);
+    void sendArgumentFloat(float value);
 
-  // Only returns the response code. If you still need 
-  // the response text (unparsed), call getResponseText()
-  short readResponse() {
-    String argv[0];
-    int argc;
-    return readResponse(argv, 0, &argc);
-  };
+    // Only returns the response code. If you still need 
+    // the response text (unparsed), call getResponseText()
+    short readResponse() {
+        String argv[0];
+        int argc;
+        return readResponse(argv, 0, &argc);
+    };
 
-  // Returns the response code
-  short readResponse(String arguments[], int max_argument_count, int *argument_count);
-  
-  short parseResponse(String response, String arguments[], int max_argument_count, int *argument_count);
+    // Returns the response code
+    short readResponse(String arguments[], int max_argument_count, int *argument_count);
 
-  
-  static CodeCategories getCodeCategory(short code) { return (CodeCategories)(code / 100); };
-  static CodeTypes getCodeType(short code) { return (CodeTypes)((code % 100) / 25); };
+    short parseResponse(String response, String arguments[], int max_argument_count, int *argument_count);
 
-  static bool isCodeInfo(int code) { return getCodeType(code) == CT_INFO; };
-  static bool isCodeError(int code) { return getCodeType(code) == CT_ERROR; };
-  static bool isCodeDebug(int code) { return getCodeType(code) == CT_DEBUG; };
 
-  String getResponseText() const { return previous_response; };
+    static CodeCategories getCodeCategory(short code) { return (CodeCategories)(code / 100); };
+    static CodeTypes getCodeType(short code) { return (CodeTypes)((code % 100) / 25); };
+
+    static bool isCodeInfo(int code) { return getCodeType(code) == CT_INFO; };
+    static bool isCodeError(int code) { return getCodeType(code) == CT_ERROR; };
+    static bool isCodeDebug(int code) { return getCodeType(code) == CT_DEBUG; };
+
+    String getResponseText() const { return previous_response; };
 private:
-  void sendArgumentStart();
-  void writeString(String value);
+    void sendArgumentStart();
+    void writeString(String value);
 
-  Stream *serial_port;
-  bool command_with_arguments;
-  String previous_response;
-  short previous_response_code;
-  String last_command;
-  
+    Stream *serial_port;
+    bool command_with_arguments;
+    String previous_response;
+    short previous_response_code;
+    String last_command;
+
 };
 
 void writeString(Stream *stream, String value);
@@ -112,4 +113,5 @@ void flushInput(Stream *stream, Stream *debug);
 // Debug IO between the Arduino and the Hiber modem
 void setDebugSerial(Stream* stream);
 void toggleDebugIO(bool enable);
-  
+
+#endif /* HIBER_H_ */
